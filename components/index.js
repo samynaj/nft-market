5ac483a9ead263585cac1a2a9a5c09dbe2bd137f
samynaj <samuelnnj@gmail.com>
2022-04-18 18:42:14 +0100

    index component

---
+++ components/index.js
@@ -0,0 +1,8 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusbar from "./FocusedStatusbar";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+
+export {
+    HomeHeader, FocusedStatusbar, NFTCard, CircleButton, RectButton
+}
